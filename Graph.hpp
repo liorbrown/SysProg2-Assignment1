ce0942f1d89c3d9cc9203badb61e6ca1172cf9fe
LiorBrown <liorbrown26@gmail.com>
2025-04-02 10:46:14 +0200

    end of graph.cpp

--- Graph.hpp
+++ Graph.hpp
@@ -1,50 +1,76 @@
 #pragma once
 
+#include "iostream"
+
 namespace graph{
-    class Edge{
-        private:
-            const static int DEFAULT_W = 1;
-            int weight;
-            Vertex* v1;
-            Vertex* v2;
-            Edge* nextEdge;
-            Edge* nextVertexEdge;
- 
-        public:
-            Edge(Vertex* v1, Vertex* v2, int weight):
-                v1(v1), 
-                v2(v2), 
-                weight(weight), 
-                nextEdge(nullptr),
-                nextVertexEdge(nullptr){}
-            Edge(Vertex* v1, Vertex* v2) {Edge(v1,v2,DEFAULT_W);}
-    };
+    class EdgeNode;
 
     class Vertex{
         private:
             int degree;
-            Edge* edges;
+            EdgeNode* edges;
 
         public:
             Vertex():degree(0), edges(nullptr){}
+            ~Vertex();
+
+            EdgeNode* getEdges() const { return this->edges;}
+            EdgeNode*& getEdges() { return this->edges;}
+    };
+
+    class Edge{
+
+        private:
+            int weight;
+            unsigned int v1;
+            unsigned int v2;
+       
+        public:
+            Edge(unsigned int v1, unsigned int v2, int weight):
+                v1(v1), 
+                v2(v2), 
+                weight(weight){}
+            
+            unsigned int getV1() const {return this->v1;}
+            unsigned int getV2() const {return this->v2;}
+            int getWeight() const {return this->weight;}
+            void print_edge() const {
+                std::cout << this->v1 << " 🔗 " << this->v2 << std::endl;
+            }
+    };
+
+    class EdgeNode{
+        private:
+            Edge* edge;
+            EdgeNode* next;
+
+        public:
+            EdgeNode(Edge* edge): edge(edge), next(nullptr){}
+            Edge* getEdge(){ return this->edge;}
+            EdgeNode* getNext() const { return this->next;}
+            EdgeNode*& getNext(){ return this->next;}
     };
 
     class Graph{
         private:
+            const static int DEFAULT_W = 1;
             int negativeEdges;
-            int nCount;
+            int nVertices;
             Vertex* vertices;
-            Edge* edges;
+            EdgeNode* edges;
+            void addEdge(Edge*, EdgeNode*&);
+            void removeEdge(EdgeNode*, EdgeNode**);
             
         public:
-            Graph(int nVertices): 
-                nCount(nVertices), 
+            Graph(int nVertices):
+                nVertices(nVertices),
                 vertices(new Vertex[nVertices]), 
                 edges(nullptr),
                 negativeEdges(0){}
-            bool addEdge(int v1, int v2, int weight);
-            bool addEdge(int v1, int v2);
-            void removeEdge(int v1, int v2);
+            ~Graph();
+            void addEdge(unsigned int, unsigned int, int);
+            void addEdge(unsigned int v1, unsigned int v2) {this->addEdge(v1, v2, DEFAULT_W);}
+            void removeEdge(unsigned int, unsigned int);
             void print_graph() const;
     };
 }
