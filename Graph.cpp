6aee754856109a178139d6995934fef4f20e5ce7
LiorBrown <liorbrown26@gmail.com>
2025-04-05 23:17:25 +0200

    Finisg documantation for Graph.hpp/cpp

--- Graph.cpp
+++ Graph.cpp
@@ -5,13 +5,18 @@
 using namespace std;
 
 namespace graph{
+    // ------------ Vertex implementaion ------------------
+    
     Vertex::~Vertex(){
         EdgeNode* currEdge = this->edges;
 
+        // Run on all edges of this vertex
         while (currEdge)
         {
             EdgeNode* next = currEdge->getNext();
 
+            // Note that here we delete only the EdgeNode and not the itself
+            // because maybe the edge itself is still use and pointed by other lists
             delete currEdge;
 
             currEdge = next;
@@ -21,25 +26,24 @@ namespace graph{
     Vertex** Vertex::getAdjacents() const{
 
         Vertex** result = new Vertex*[this->degree];
-        EdgeNode* currEdge = this->edges;
+        int i = 0;
 
-        for (size_t i = 0; i < this->degree ; i++){
-            Vertex* currVertex = currEdge->getEdge()->getAdjacent(this);
-            result[i] = currVertex;
-            currEdge = currEdge->getNext();
-        }
+        // Run on all vertex edges
+        for (EdgeNode* e = this->edges; e ; e = e->getNext())
+            // Add to array the adjacent vertex in the current edge
+            result[i++] = e->getEdge()->getAdjacent(this);
         
         return result;
     }
 
     int Vertex::getWeight(const Vertex* v) const{
-        for (EdgeNode* e = this->edges; e ; e = e->getNext()){
-            Edge edge = *e->getEdge();
 
-            if (edge.getV1() == v || edge.getV2() == v)
-                return edge.getWeight();
-        }
-        
+        // Run on all vertex edges
+        for (EdgeNode* e = this->edges; e ; e = e->getNext())
+            // Check if v is the adjacent of this in current edge
+            if (e->getEdge()->getAdjacent(this) == v)
+                return e->getEdge()->getWeight();
+            
         throw runtime_error{"The vertices are not adjacents"};
     }
 
@@ -47,20 +51,26 @@ namespace graph{
         Vertex* thisRoot = this;
         Vertex* vRoot = v; 
         
+        // Step up in this vertex tree until reach its root
         while (thisRoot->getNext())
             thisRoot = thisRoot->getNext();
 
+        // Step up in given vertex tree until reach its root
         while (vRoot->getNext())
             vRoot = vRoot->getNext();
         
+        // Check if they have the same root, so they called "Union"
         if (thisRoot == vRoot)
             return true;
+        // If not have the same root, then union their roots and return false
+        // because they "where" not union
         else{
             thisRoot->getNext() = vRoot;
             return false;
         }
     }
 
+    // ------------ Graph implementaion ------------------
     Graph::Graph(const unsigned int nVertices):
         nVertices(nVertices),
         vertices(new Vertex[nVertices]), 
@@ -68,21 +78,26 @@ namespace graph{
         negativeEdges(0),
         nEdges(0){
         
+        // Runs on all graph vertices and sets their id to their index
         for (size_t i = 0; i < nVertices; i++)            
             (*this)[i].getID() = i;
     }
 
     Graph::~Graph(){
+        // Delete vertices array
         delete[] this->vertices;
-        this->vertices = nullptr;
 
         EdgeNode* currEdge = this->edges;
 
-        while (currEdge)
-        {
+        // Run on all graph edges
+        while (currEdge){
             EdgeNode* next = currEdge->getNext();
 
+            // Delete edge itself. Note that all other EdgeNode that contains this edge was already deleted
+            // when we write "delete[] this->vertices", so we can safety delete this edge
             delete currEdge->getEdge();
+
+            // Deletes edge node
             delete currEdge;
 
             currEdge = next;
@@ -96,17 +111,25 @@ namespace graph{
         if (v1 == v2)
             throw invalid_argument{"This graph is simple, edge from vertex to itself is not allowed 🙄"};
 
+        // Run on all current edges of v1 for check if edge already exist
         for (EdgeNode* e = (*this)[v1].getEdges(); e ; e = e->getNext())
+            // Because we dont know if our v1 is also v1 in the edge or its v2
+            // we need to check both
             if (e->getEdge()->getV1() == &(*this)[v2] || 
                 e->getEdge()->getV2() == &(*this)[v2])
                 throw invalid_argument{"Edge already exist 🙄"};
         
+        // Creates new edge
         Edge* newEdge = new Edge(&(*this)[v1], &(*this)[v2], weight);
 
+        // Adds new edge to graph edges list
         this->addEdge(newEdge, this->edges);
+
+        // Adds new edge to both vertices edges list, and increment their degree
         this->addEdge(newEdge, (++((*this)[v1])).getEdges());
         this->addEdge(newEdge, (++((*this)[v2])).getEdges());
 
+        // We have to update amount of negative edges, for know any time if there are negative edges
         if (weight < 0)
             this->negativeEdges++;
         
@@ -114,7 +137,11 @@ namespace graph{
     }
 
     void Graph::addEdge(Edge* newEdge, EdgeNode*& edges){
+
+        // Creates new EdgeNode
         EdgeNode* newEdgeNode = new EdgeNode(newEdge);
+
+        // Puts new EdgeNode in the head of given edges list
         newEdgeNode->getNext() = edges;
         edges = newEdgeNode; 
     }
@@ -126,33 +153,50 @@ namespace graph{
         if (v1 == v2)
             throw invalid_argument{"This graph is simple, edge from vertex to itself is not allowed 🙄"};
 
+        // Set pointer to pointer of previous edge,
+        // for make its point to next edge
         EdgeNode** previous = &(*this)[v1].getEdges();
         EdgeNode* currEdge = *previous;
 
+        // Runs on all v1 edges and searce for the edge that connect to v2
         while (currEdge){
             const Edge* edge = currEdge->getEdge();
 
-            if (edge->getV1() == &(*this)[v2] || edge->getV2() == &(*this)[v2]){ 
-                
+            // Checks if the adjacent of v1 in this edge is v2
+            if (edge->getAdjacent(&(*this)[v1]) == &(*this)[v2]){
+
+                // Removes edge from graph edge list
                 this->removeEdge(edge, &this->edges);
+
+                // Removes edge from v2 edge list, and decrease its degree
                 this->removeEdge(edge, &(--((*this)[v2])).getEdges());
 
                 if (edge->getWeight() < 0)
                     this->negativeEdges--;
                 
                 this->nEdges--;
-                
+
+                // Delete edge itself
                 delete edge;
 
                 EdgeNode* next = currEdge->getNext();
+
+                // Delete v1 EdgeNode
                 delete currEdge;
+
+                // Sets previous EdgeNode to point to next edge
                 *previous = next;
                 
-                --this->vertices[v1];
+                // Decrease v1 degree
+                --((*this)[v1]);
 
                 return;
+            // If current edge is not connect to v2
             } else{
+                // Sets previous to pint to pointer of next vertex
                 previous = &currEdge->getNext();
+
+                // Continue to next edge
                 currEdge = *previous;
             }
         } 
@@ -163,15 +207,25 @@ namespace graph{
     void Graph::removeEdge(const Edge* edge, EdgeNode** previous){
         EdgeNode* currEdge = *previous;
 
+        // Runs on all edges in given list
         while (currEdge){
+            // Check if current edge is given edge
             if (currEdge->getEdge() == edge){
                 EdgeNode* next = currEdge->getNext();
+
+                // Delete current EdgeNode
                 delete currEdge;
+
+                // Sets previous EdgeNode to point to next edge
                 *previous = next;
 
                 return;
+            // If current edge isn't given edge
             } else {
+                // Sets previous to pint to pointer of next vertex
                 previous = &currEdge->getNext();
+
+                // Continue to next edge
                 currEdge = *previous;
             }
         }
@@ -181,6 +235,7 @@ namespace graph{
         cout << "The graph have " << this->nVertices << " vertices" << endl;
         cout << "Those are the graph edges:" << endl;
 
+        // Runs on all graph edges, and print them
         for (EdgeNode* e = this->edges ; e ; e = e->getNext())
             cout << *e->getEdge() << endl;
     }
@@ -190,9 +245,11 @@ namespace graph{
         Edge** result = new Edge*[this->nEdges];
         int i = 0;
     
+        // Init edges array with all graph edges
         for (EdgeNode* e = this->edges; e ; e = e->getNext())
             result[i++] = e->getEdge();
     
+        // Bubble sort edge array by weight
         bool isSwap = true;
     
         for (size_t i = 0; i < this->nEdges - 1 && isSwap; i++){
@@ -214,6 +271,7 @@ namespace graph{
 
     void Graph::clearVertices() const
     {
+        // Runs on all graph vertices and clear their next and prev attributes
         for (size_t i = 0; i < this->nVertices; i++)
             this->vertices[i].getNext() = this->vertices[i].getPrev() = nullptr;
     }
--- Graph.hpp
+++ Graph.hpp
@@ -5,67 +5,115 @@
 using namespace std;
 
 namespace graph{
+
+    // This is enum is used in BF & DFS algorithms
     enum vColor{
         WHITE, GRAY, BLACK
     };
 
+    // This "strange" declaration is needed because Vertex is depend on EdgeNode
+    // but EdgeNode depend on Edge and Edge depend on vertx
+    // so the solution for this plonter is to declare EdgeNode twice
     class EdgeNode;
 
+    /// @brief this class represent vertex in graph
+    /// the class include basic data of vertex like id and degree
+    /// and also enhanced data like pi, d & color that is used for algorithms
     class Vertex{
         private:
+            // Basic data
             unsigned int id;
             unsigned int degree;
             EdgeNode* edges;
+
+            // Enhanced data for algorithms usage
             unsigned int d;
             Vertex* pi;
             vColor color;
+
+            // Data that used for data structures 
+            // that contains the vertex like heap, queue , etc.
             Vertex* next;
             Vertex* prev;
+
+            // I aware that this not pure oop practice that object know the structure that contains it
+            // but i prefer this for get better efficiency in the algorithm
             bool isInHeap;
 
         public:
-            Vertex():degree(0), edges(nullptr), next(nullptr), prev(nullptr), isInHeap(false){}
+            Vertex():
+                degree(0), 
+                edges(nullptr), 
+                next(nullptr), 
+                prev(nullptr), 
+                isInHeap(false){}
+
+            /// @brief Destructor - delete vertex edges list
             ~Vertex();
 
+            // Geters & seters
             unsigned int getID() const {return this->id;}
             unsigned int& getID() {return this->id;}
+            unsigned int getDegree() const {return this->degree;}
             EdgeNode* getEdges() const { return this->edges;}
             EdgeNode*& getEdges() { return this->edges;}
-            unsigned int getDegree() const {return this->degree;}
-            Vertex** getAdjacents() const;
-            Vertex* getNext() const {return this->next;}
-            Vertex*& getNext(){return this->next;}
-            Vertex* getPrev() const {return this->prev;}
-            Vertex*& getPrev(){return this->prev;}
+            
             unsigned int getD() const {return this->d;}
             unsigned int& getD() {return this->d;}
             Vertex* getPI() const {return this->pi;}
             Vertex*& getPI() {return this->pi;}
             vColor getColor() const {return this->color;}
             vColor& getColor() {return this->color;}
+
+            Vertex* getNext() const {return this->next;}
+            Vertex*& getNext(){return this->next;}
+            Vertex* getPrev() const {return this->prev;}
+            Vertex*& getPrev(){return this->prev;}
             bool getIsInHeap() const {return this->isInHeap;}
             bool& getIsInHeap() {return this->isInHeap;}
-            int getWeight(const Vertex*) const;
-            bool isUnioun(Vertex*); 
 
+            /// @brief Get all adjacents vertices of this vertex
+            /// @return Array of pointers to adjacents vertices
+            Vertex** getAdjacents() const;
+            
+            /// @brief Get the weight of the edge that connect this vertex to given vertex
+            /// @param  v Vertex to check weight of edge to it
+            /// @return the weight of the edge that connect this vertex to given vertex
+            int getWeight(const Vertex* v) const;
+
+            /// @brief Check if this and given vertices are in the same group, in terms of union-find groups,
+            /// and also union the to vertices groups in the end to one group.
+            /// NOTE: Actualy this function is implement all the union-find structure wtihout really create 
+            /// union-find structure. this possible only because the assingment not requierd get real union-find
+            /// complexity, so i do it realy simple in one function that  give me exactly 
+            /// the same functionality of union-find structure
+            /// @param v Vertex to check and union this vertex with
+            /// @return True - if vertices "was" in same group before function, False otherwise
+            /// in any case in the end of function they will be in same grouop
+            bool isUnioun(Vertex* v); 
+
+            /// @brief This operator increase vertex degree
+            /// @return This vertex "After" increasment
             Vertex& operator++(){
                 this->degree++;
                 return *this;
             }
 
+            /// @brief This operator decrease vertex degree
+            /// @return THis vertex "After" decreasment
             Vertex& operator--(){
                 this->degree--;
                 return *this;
             }
     };
 
+    /// @brief This class represented edge in graph
     class Edge{
 
         private:
             const int weight;
             Vertex* v1;
             Vertex* v2;
-            bool isSorted;
        
         public:
             Edge(Vertex* v1, Vertex* v2, const int weight):
@@ -73,18 +121,29 @@ namespace graph{
                 v2(v2), 
                 weight(weight){}
             
+            // Geters only. no changes aloowed once edge created
             int getWeight() const {return this->weight;}
             Vertex* getV1() const {return this->v1;}
             Vertex* getV2() const {return this->v2;}
-            bool getIsSorted() const {return this->isSorted;}
-            bool& getIsSorted() {return this->isSorted;}
+
+            /// @brief This function gets one vertex of this edge, and return the other one 
+            /// @param v The vertex to check its neighbor
+            /// @return The other vertex of this edge
             Vertex* getAdjacent(const Vertex* v) const{
                 return (this->v1 == v ? this->v2 : this->v1);
             }
 
+            /// @brief Output operator for print edge data
+            /// @param os stream to move data to it
+            /// @param e Edge o get its data
+            /// @return The given stream
             friend ostream& operator<<(ostream& os, const Edge& e);
     };
   
+    /// @brief This class is container of the Edge class, and its purpose is to enabled creating of
+    /// edges linked lists. note that because we want that the same edge could be linked in different lists
+    /// like the graph general list and the vertices edges lists, so we have have to sepperate the edge itself
+    /// from the node in list. thus we can create one edge, and list it in many lists
     class EdgeNode{
         private:
             Edge* edge;
@@ -92,35 +151,76 @@ namespace graph{
 
         public:
             EdgeNode(Edge* edge): edge(edge), next(nullptr){}
+
+            // Geters & setter
             Edge* getEdge() const { return this->edge;}
             EdgeNode* getNext() const { return this->next;}
             EdgeNode*& getNext(){ return this->next;}
     };
 
+    /// @brief This class represent a simple a-directed weighted graph
+    /// with const amount of vertices
     class Graph{
         private:
             const static int DEFAULT_W = 1;
+
             unsigned int nEdges;
             unsigned int negativeEdges;
             const unsigned int nVertices;
             Vertex* vertices;
             EdgeNode* edges;
 
-            void addEdge(Edge*, EdgeNode*&);
-            void removeEdge(const Edge*, EdgeNode**);
+            /// @brief This method adds new edge to given edges list
+            /// @param e The new edge to add
+            /// @param list The edges list to ass new edge to it
+            void addEdge(Edge* e, EdgeNode*& list);
+
+            /// @brief This method remove given edge from given edges list
+            /// @param e Edge to remove
+            /// @param Pointer to edge list to remove from
+            void removeEdge(const Edge* e, EdgeNode** previous);
             
         public:
             Graph(const unsigned int nVertices);
+
+            /// @brief Destructor - delete edges list
             ~Graph();
-            void addEdge(const unsigned int, const unsigned int, const int weight = DEFAULT_W);
-            void removeEdge(const unsigned int, const unsigned int);
-            void print_graph() const;
+
+            // Geters
             unsigned int getnVertices() const {return this->nVertices;}
             unsigned int getnEdges() const {return this->nEdges;}
-            Edge** getSortedEdges() const;
+
+            /// @brief Check if graph have negative edges
+            /// @return True if have negative edges, False otherwise
             bool isNegative() const {return this->negativeEdges;}
-            bool isConnected() const;
+            
+            /// @brief This method add to graph new edge between 2 given vertices
+            /// @param v1 Vertex 1 id
+            /// @param v2 Vertex 2 id
+            /// @param weight Edge weight. Default = 1
+            void addEdge(const unsigned int v1, const unsigned int v2, const int weight = DEFAULT_W);
+
+            /// @brief This method remove the edge between 2 vertices from graph
+            /// @param v1 Vertex 1 id
+            /// @param v2 Vertex 2 id
+            void removeEdge(const unsigned int v1, const unsigned int v2);
+
+            /// @brief Print graph data
+            void print_graph() const;
+            
+            /// @brief Creates array of pointers to all graph edges, sorted ASC by edges weight 
+            /// @return The sorted array
+            Edge** getSortedEdges() const;
+            
+            /// @brief This method sets to null the next and prev members of all vertices.
+            /// this need to be called before creating of structures like queue and heap
+            /// that using those attributes, to avoid irrelevant prior data
             void clearVertices() const;
+
+            /// @brief This [] operator גesigned to make it easier to access vertices, 
+            /// as this is a very repetitive operation.
+            /// @param v Vertex id
+            /// @return The wanted vertex
             Vertex& operator[](const int v) const {return this->vertices[v];}
     };
 }
